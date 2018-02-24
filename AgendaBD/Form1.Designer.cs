324bc479a549055259990bc3facf42b1b058d237
Marcio Martins <marciopmm@gmail.com>
2018-02-24 12:02:21 -0300

    Primeira carga

--- AgendaBD/Form1.Designer.cs
+++ AgendaBD/Form1.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grpContatos = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDataNasc = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.grpContatos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpContatos
+            // 
+            this.grpContatos.Controls.Add(this.txtEmail);
+            this.grpContatos.Controls.Add(this.txtCelular);
+            this.grpContatos.Controls.Add(this.txtNome);
+            this.grpContatos.Controls.Add(this.lblID);
+            this.grpContatos.Controls.Add(this.txtDataNasc);
+            this.grpContatos.Controls.Add(this.label5);
+            this.grpContatos.Controls.Add(this.label4);
+            this.grpContatos.Controls.Add(this.label3);
+            this.grpContatos.Controls.Add(this.label2);
+            this.grpContatos.Controls.Add(this.label1);
+            this.grpContatos.Location = new System.Drawing.Point(13, 13);
+            this.grpContatos.Name = "grpContatos";
+            this.grpContatos.Size = new System.Drawing.Size(484, 246);
+            this.grpContatos.TabIndex = 0;
+            this.grpContatos.TabStop = false;
+            this.grpContatos.Text = "Contatos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Data de Nascimento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Celular";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Email";
+            // 
+            // txtDataNasc
+            // 
+            this.txtDataNasc.Location = new System.Drawing.Point(117, 92);
+            this.txtDataNasc.Name = "txtDataNasc";
+            this.txtDataNasc.Size = new System.Drawing.Size(76, 20);
+            this.txtDataNasc.TabIndex = 5;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(117, 29);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(16, 13);
+            this.lblID.TabIndex = 6;
+            this.lblID.Text = "---";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(117, 59);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(217, 20);
+            this.txtNome.TabIndex = 7;
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Location = new System.Drawing.Point(117, 126);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(76, 20);
+            this.txtCelular.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(117, 159);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(217, 20);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(508, 271);
+            this.Controls.Add(this.grpContatos);
+            this.Name = "Form1";
+            this.Text = "Agenda";
+            this.grpContatos.ResumeLayout(false);
+            this.grpContatos.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpContatos;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtDataNasc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
